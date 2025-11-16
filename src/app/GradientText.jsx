c73fba77482155436824ca20efb784a28caf23cc
Clarity <claridadjulyfranz@gmail.com>
2025-11-16 19:50:42 +0800

    Delete src directory

--- src/app/GradientText.jsx
+++
@@ -1,7 +0,0 @@
-export default function GradientText({ children }) {
-  return (
-    <span className={`bg-clip-text text-transparent animate-gradient ${className}`} style={style}>
-      {children}
-    </span>
-  );
-}
